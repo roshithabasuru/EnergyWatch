b55d605928cc2882046da8ece1022f7f4046cc6f
roshithabasuru <roshithabasuru@gmail.com>
2026-05-15 22:02:18 +0530

    Add comment to create_db.py

--- create_db.py
+++ create_db.py
@@ -5,3 +5,4 @@ engine = create_engine('mysql://root:root@localhost:3306/')
 with engine.connect() as conn:
     conn.execute(text("CREATE DATABASE IF NOT EXISTS energywatch CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"))
 print("Database created successfully")
+#comment
